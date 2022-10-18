5e4ed70359b2b575d5b0d5246284034619745061
German Beldorati Stark <gerbeldo@gmail.com>
2022-10-18 18:01:31 -0300

    test gem2s 7

--- pipeline-runner/tests/testthat/test-pipeline.R
+++ pipeline-runner/tests/testthat/test-pipeline.R
@@ -53,6 +53,67 @@ stubbed_download_user_files <- function(input, pipeline_config, prev_out = list(
 }
 
 stub_put_object_in_s3 <- function(pipeline_config, bucket, object, key){
+  if (!dir.exists(bucket)) dir.create(bucket)
+  writeBin(object, file.path(bucket, key))
+}
+
+stub_remove_bucket_folder <- function(pipeline_config, bucket, folder) {
+  # we cleanup inside the corresponding tests
+}
+
+stub_tempdir <- function() {
+  # stub to write always to the same place and be able to capture written files
+  # consistently
+  base_path <- ifelse(basename(getwd()) == "pipeline-runner",
+                      "./tests/testthat",
+                      ".")
+
+  mock_path <- file.path(base_path,
+                         "mock_data")
+
+  temp_path <- file.path(mock_path, "temp")
+
+  if(!dir.exists(temp_path)) dir.create(temp_path, recursive = T)
+
+  return(temp_path)
+}
+
+stub_put_object_in_s3_multipart <- function(pipeline_config, bucket, object, key){
+  dir_path <- file.path(bucket, dirname(key))
+  if (!dir.exists(dir_path)) dir.create(dir_path, recursive = T)
+  file.copy(object, file.path(bucket, key))
+}
+
+stubbed_upload_to_aws <- function(input, pipeline_config, prev_out) {
+  mockery::stub(upload_to_aws, "put_object_in_s3", stub_put_object_in_s3)
+  mockery::stub(upload_to_aws, "remove_bucket_folder", stub_remove_bucket_folder)
+  mockery::stub(upload_to_aws, "tempdir", stub_tempdir)
+  mockery::stub(upload_to_aws, "put_object_in_s3_multipart", stub_put_object_in_s3_multipart)
+
+  upload_to_aws(input, pipeline_config, prev_out)
+
+}
+
+mock_pipeline_config <- function(development_aws_server = "mock_aws_server") {
+  local_envvar(list("AWS_ACCOUNT_ID" = "000000000000",
+                    "ACTIVITY_ARN" = "mock_arn"))
+
+  pipeline_config <- load_config(development_aws_server)
+
+  base_path <- ifelse(basename(getwd()) == "pipeline-runner",
+                      "./tests/testthat",
+                      ".")
+
+
+  mock_path <- file.path(base_path,
+                         "mock_data")
+
+  # replace buckets with the local path
+  for (bucket in grep("bucket$", names(pipeline_config), value = T)) {
+    pipeline_config[[bucket]] <- file.path(mock_path, bucket)
+  }
+
+  return(pipeline_config)
 
 }
 
@@ -68,8 +129,7 @@ test_that("gem2s-1 - donwload files downloads 10x files", {
   input <-
     RJSONIO::fromJSON(file.path(mock_path, "input/input.json"))
 
-  pipeline_config <-
-    list(originals_bucket = file.path(mock_path, "mock_bucket"))
+  pipeline_config <- mock_pipeline_config()
 
   res <- stubbed_download_user_files(input, pipeline_config)
 
@@ -225,5 +285,50 @@ test_that("gem2s-6 - prepares experiment", {
   saveRDS(res, out_path)
   expect_snapshot_file(out_path, name = paste("gem2s-6", basename(out_path), sep = "-"))
   defer_parent(unlink(out_path))
+
+})
+
+
+test_that("gem2s-7 - creates cellsets and uploads to AWS", {
+  base_path <- ifelse(basename(getwd()) == "pipeline-runner",
+                      "./tests/testthat",
+                      ".")
+
+  mock_path <- file.path(base_path,
+                         "mock_data")
+
+  input <-
+    RJSONIO::fromJSON(file.path(mock_path, "input/input.json"))
+
+  pipeline_config <- mock_pipeline_config()
+  prev_out <-
+    readRDS(file.path(base_path, "_snaps/pipeline", "gem2s-6-out.rds"))
+  prev_out <- prev_out$output
+
+  res <- stubbed_upload_to_aws(input, pipeline_config, prev_out)
+
+  # cellsets file
+  expect_snapshot_file(
+    file.path(pipeline_config$cell_sets_bucket, input$experimentId),
+    name = paste("gem2s-7", input$experimentId, "cellsets.json", sep = "-")
+  )
+  withr::defer(unlink(pipeline_config$cell_sets_bucket, recursive = TRUE),
+               envir = parent.frame())
+
+  # raw sample seurat objects
+  for (sample_id in prev_out$config$samples) {
+    expect_snapshot_file(
+      file.path(
+        pipeline_config$source_bucket,
+        input$experimentId,
+        sample_id,
+        "r.rds"
+      ),
+      name = paste("gem2s-7", input$experimentId, sample_id, "r.rds", sep = "-")
+    )
+  }
+  withr::defer(unlink(pipeline_config$source_bucket, recursive = T))
+  withr::defer(unlink(file.path(mock_path, "temp"), recursive = T))
+
 
 })
