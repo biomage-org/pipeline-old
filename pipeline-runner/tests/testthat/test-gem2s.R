fa5b5381d29c2e4621c4da0f01d4cf329d252a13
German Beldorati Stark <gerbeldo@gmail.com>
2022-10-24 18:32:16 -0300

    move repeated stubbers to a separate file

--- pipeline-runner/tests/testthat/test-gem2s.R
+++ pipeline-runner/tests/testthat/test-gem2s.R
@@ -1,77 +1,8 @@
-stub_s3_list_objects <- function(Bucket, Prefix) {
-  # this workaround is the lesser evil ("bucket/./project/sample")
-  Prefix <- gsub("^./", "", Prefix)
-
-  # returns list with structure like s3$list_objects, but with mocked paths
-  files <-
-    list.files(file.path(Bucket, Prefix),
-               full.names = TRUE,
-               recursive = TRUE)
-  l <- as.list(files)
-  names(l) <- NULL
-  l2 <- lapply(l, list)
-
-  for (i in seq_along(l2)) {
-    names(l2[[i]]) <- "Key"
-  }
-  list(Contents = l2)
-}
-
-
-stub_s3_get_object <- function(Bucket, Key) {
-  # returns a list of the raw file read from the mocked s3 bucket.
-  out <- list(body = readBin(file.path(Bucket, Key),
-                             what = "raw", n = 120000000L),
-              rest = list())
-  return(out)
-}
-
-
-stub_file.path <- function(...) {
-  file.path(".", ...)
-}
-
-stubbed_download_user_files <-
-  function(input, pipeline_config, prev_out = list()) {
-    # helper to simplify calls to the stubbed function
-
-    mockedS3 <- list(list_objects = stub_s3_list_objects,
-                     get_object = stub_s3_get_object)
-
-    # where makes sure where we are stubbing the what calls.
-    mockery::stub(where = download_user_files,
-                  what = "paws::s3",
-                  how = mockedS3)
-    mockery::stub(get_gem2s_file, "s3$list_objects", mockedS3$list_objects)
-
-    mockery::stub(download_user_files, "file.path", stub_file.path)
-    mockery::stub(get_gem2s_file, "file.path", stub_file.path)
-
-    mockery::stub(download_and_store, "s3$get_object", mockedS3$get_object)
-
-    res <-
-      download_user_files(input,
-                          pipeline_config,
-                          input_dir = "./input",
-                          prev_out = prev_out)
-    # download_user_files creates a "/input" folder in the pod. defer deleting
-    # it during tests.
-    withr::defer(unlink("./input", recursive = TRUE), envir = parent.frame())
-
-    res
-  }
-
-stub_put_object_in_s3 <-
-  function(pipeline_config, bucket, object, key) {
-    if (!dir.exists(bucket))
-      dir.create(bucket)
-    writeBin(object, file.path(bucket, key))
-  }
-
 stub_remove_bucket_folder <-
   function(pipeline_config, bucket, folder) {
     # we cleanup inside the corresponding tests
   }
+
 
 stub_tempdir <- function() {
   # stub to write always to the same place and be able to capture written files
@@ -91,6 +22,7 @@ stub_tempdir <- function() {
   return(temp_path)
 }
 
+
 stub_put_object_in_s3_multipart <-
   function(pipeline_config, bucket, object, key) {
     # if we do not test the raw RDSs uploaded by upload_to_aws, we can remove
@@ -100,6 +32,7 @@ stub_put_object_in_s3_multipart <-
       dir.create(dir_path, recursive = T)
     file.copy(object, file.path(bucket, key))
   }
+
 
 stubbed_upload_to_aws <-
   function(input, pipeline_config, prev_out) {
@@ -115,6 +48,7 @@ stubbed_upload_to_aws <-
     upload_to_aws(input, pipeline_config, prev_out)
 
   }
+
 
 mock_pipeline_config <-
   function(development_aws_server = "mock_aws_server") {
@@ -165,6 +99,7 @@ path_setup <- function() {
 
 }
 
+
 make_snapshot_name <- function(step_n, experiment_id, output_name) {
   paste("gem2s", step_n, experiment_id, paste0(output_name, ".R"), sep = "-")
 }
@@ -178,6 +113,7 @@ source_prev_out <- function(paths, current_step, experiment_id, output_name = "o
                                       output_name)),
          local = parent.frame())
 }
+
 
 step_setup <- function(step_n, experiment_id) {
 
@@ -197,6 +133,7 @@ step_setup <- function(step_n, experiment_id) {
   }
   return(output)
 }
+
 
 test_gem2s <- function(experiment_id) {
 
